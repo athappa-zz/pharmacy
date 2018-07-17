222fc8ce8b0381f306cc1318e3bf13954df79a7d
Andrew  Thappa <adthappa@gmail.com>
2018-07-17 11:17:32 -0700

    Update

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
+
+
+# Test Shell Scripts
+# python ./src/pharmacy_counting.py ./insight_testsuite/tests/test_1/input/itcont.txt 
+#  ./output/top_cost_drug_test.txt
+
+
+# test_1
+python ./src/pharmacy_counting.py ./insight_testsuite/tests/test_1/input/itcont.txt ./insight_testsuite/tests/test_1/output/top_cost_drug_test.txt
+
+# python ./src/pharmacy_counting.py ./input/de_cc_data.txt ./output/top_cost_drug.txt
+
+
